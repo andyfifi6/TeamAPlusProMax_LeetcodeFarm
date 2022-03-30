c294cd2eee80203453949a35cc02df51c52dd209
andyfifi6 <andyfifi6@gmail.com>
2022-03-30 15:35:28 -0700

    Leetcode Farm

---
+++ app/src/main/java/neu/finalPro/LeetcodeFarm/user/MainActivity.java
@@ -0,0 +1,16 @@
+package neu.finalPro.LeetcodeFarm.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import neu.finalPro.LeetcodeFarm.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
