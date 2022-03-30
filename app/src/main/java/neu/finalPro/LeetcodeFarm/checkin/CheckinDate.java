7183c14f1e94a7f6074739570131d99839060c74
andyfifi6 <andyfifi6@gmail.com>
2022-03-30 15:40:05 -0700

    Leetcode Farm

---
+++ app/src/main/java/neu/finalPro/LeetcodeFarm/checkin/CheckinDate.java
@@ -0,0 +1,16 @@
+package neu.finalPro.LeetcodeFarm.checkin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import neu.finalPro.LeetcodeFarm.R;
+
+public class CheckinDate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkin_date);
+    }
+}
